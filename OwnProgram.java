432ce309f570e46021d0ef9c50816c135d811546
Will <w.calhoun@outlook.com>
2020-11-10 23:34:11 -0800

    first commit Java

---
+++ OwnProgram.java
@@ -0,0 +1,7 @@
+public class OwnProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Will Calhoun");
+        System.out.println("I am 28 years old");
+        System.out.println("My hometown is Albuquerque, New Mexico");
+    }
+}
